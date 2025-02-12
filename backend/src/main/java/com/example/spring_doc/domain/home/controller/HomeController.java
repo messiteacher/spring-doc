19c206f39a91b3a28e3a96e4593443fcf92e0cd9
ShinYounHo <suh2963@gmail.com>
2025-02-12 11:42:29 +0900

    home conroller design

--- backend/src/main/java/com/example/spring_doc/domain/home/controller/HomeController.java
+++ backend/src/main/java/com/example/spring_doc/domain/home/controller/HomeController.java
@@ -1,12 +1,16 @@
 package com.example.spring_doc.domain.home.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Tag(name = "HomeController", description = "API 서버 홈")
 @Controller
 public class HomeController {
 
+    @Operation(summary = "API 서버 시작페이지", description = "API 서버 시작페이지입니다. api 호출은 인증을 해주세요")
     @GetMapping("/")
     @ResponseBody
     public String home() {
